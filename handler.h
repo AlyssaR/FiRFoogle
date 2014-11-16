62663c2694a65161165e2606f191b8c35de5698e
AlyssaR <arahman@smu.edu>
2014-11-16 16:24:51 -0600

    I don't know what I changed here, but I'm committing it.

--- handler.h
+++ handler.h
@@ -12,16 +12,17 @@ class Handler {
 private:
     Index * index;
     BabyParser * parse;
+    vector<int> sorted(map<int, int>); //Sorts docs by weights
 public:
     Handler() {
         index = new Index();
         parse = new BabyParser(); //Change to use big boy parser later
     }
 
+    void addToIndex(char*); //Accepts filename of XML doc
     bool createIndex(char*, char*); //Accepts XML and output filename from caller
     void deleteIndex() { delete index; }
     vector<int> search(vector<string>, vector<string>, vector<string>); //Accepts keywords, returns docs in weighted order
-    vector<int> sorted(map<int, int>); //Sorts docs by weights
 
     ~Handler() {
         delete index;
